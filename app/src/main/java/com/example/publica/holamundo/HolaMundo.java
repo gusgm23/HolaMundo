c452452ad871b3ddb4dc04c02cb30192662ab3e5
gusgm33 <gustavogonzalesmosquera@gmail.com>
2019-05-03 17:50:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/publica/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.example.publica.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HolaMundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo);
+    }
+}
